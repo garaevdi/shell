c11ae27b0bfc4c44cb7e0dcf2b16f594cb19d5d6
Denis Garaev <garaevdi@yandex.ru>
2025-01-19 06:15:29 +0300

    fix: patch from upstream

--- src/panel_settings.ts
+++ src/panel_settings.ts
@@ -294,14 +294,14 @@ function parse_number(text: string): number {
     return number;
 }
 
-function toggle(desc: string, active: boolean, connect: (toggle: any) => void): any {
+function toggle(desc: string, active: boolean, connect: (toggle: any, state: boolean) => void): any {
     let toggle = new PopupSwitchMenuItem(desc, active);
 
     // @ts-ignore
     toggle.label.set_y_align(Clutter.ActorAlign.CENTER);
 
-    toggle.connect('toggled', () => {
-        connect(toggle);
+    toggle.connect('toggled', (_: any, state: boolean) => {
+        connect(toggle, state);
         return true;
     });
 
@@ -309,7 +309,13 @@ function toggle(desc: string, active: boolean, connect: (toggle: any) => void): 
 }
 
 function tiled(ext: Ext): any {
-    let t = toggle(_!('Tile Windows'), null != ext.auto_tiler, () => ext.toggle_tiling());
+    let t = toggle(_!('Tile Windows'), null != ext.auto_tiler, (_, shouldTile) => {
+        if (shouldTile) {
+            ext.auto_tile_on();
+        } else {
+            ext.auto_tile_off();
+        }
+    });
     return t;
 }
 
